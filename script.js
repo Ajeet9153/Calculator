91483caad99a768bf45d7c9896d0433bbaa915bf
Ajeet9153 <ajeetkumar4035@gmail.com>
2025-03-02 11:23:35 +0530

    Modify CSS

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let currentDisplay = "";
 
 function updateDisplay() {
-  document.querySelector("#display").value = currentDisplay;
+    document.querySelector("#display").value = currentDisplay;
 }
 
 function appendToDisplay(value) {
-  currentDisplay += value;
-  updateDisplay();
+    currentDisplay += value;
+    updateDisplay();
 }
 
 function clearDisplay() {
-  currentDisplay = "";
-  updateDisplay();
+    currentDisplay = "";
+    updateDisplay();
 }
 
 function calculateResult() {
-  try {
-    currentDisplay = eval(currentDisplay);
-    updateDisplay();
-  } catch (error) {
-    currentDisplay = "Error";
-    updateDisplay();
-  }
+    try {
+        currentDisplay = new Function('return ' + currentDisplay)();
+        updateDisplay();
+    } catch (error) {
+        currentDisplay = "Error";
+        updateDisplay();
+    }
 }
